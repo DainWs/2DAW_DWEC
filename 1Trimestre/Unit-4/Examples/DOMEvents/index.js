4f6b02fa21c84ff171c2ff4a1fa88145828a1565
DainWs <jose.ant.du@gmail.com>
2021-11-02 09:39:47 +0100

    Licence changes

--- 1Trimestre/Unit-4/Examples/DOMEvents/index.js
+++ 1Trimestre/Unit-4/Examples/DOMEvents/index.js
@@ -4,7 +4,9 @@ function random(number) {
     return Math.floor(Math.random() * (number+1));
 }
 
-btn.onclick = function() {
+function cambiarColorFondo(){
     const rndColor = 'rgb('+random(255)+','+random(255)+','+random(255)+')';
     document.body.style.backgroundColor = rndColor;
 }
+
+btn.addEventListener("click", cambiarColorFondo);
