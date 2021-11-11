08e0e65ea46d84306f9f9ab8d3e0f263cad7344e
DainWs <jose.ant.du@gmail.com>
2021-11-11 14:22:07 +0100

    Exercises

--- 1Trimestre/Unit-4/Exercises/Worksheet-04/Apartado-01/js/index.js
+++ 1Trimestre/Unit-4/Exercises/Worksheet-04/Apartado-01/js/index.js
@@ -1,26 +1,10 @@
-const coords = [0, 0];
-const lastCoords = [0, 0];
-function initial(e) {
-    coords[0] = e.screenX - lastCoords[0];
-    coords[1] = e.screenY - lastCoords[1];
-}
-
-function move(e) {
-    console.log(e);
-    if(e.screenX != 0 || e.screenY != 0) {
-        let x = (e.screenX - coords[0]);
-        let y = (e.screenY - coords[1]);
-        e.target.style = `transform: translate(${x}px,${y}px)`;
-        lastCoords[0] = x;
-        lastCoords[1] = y;
-    }
-    return true;
-}
-
 function afterLoad() {
-    let img = document.querySelector("img");
-    img.ondragstart = initial;
-    img.ondrag = move;
+    let aside = document.getElementsByName("aside")[0];
+    aside.style.style = "width: 50px;height: 100%;position: absolute;left: 0;";
+    let header = document.getElementsByName("header")[0];
+    header.style.style = "margin-left: 270px;";
+    let section = document.getElementsByName("section")[0];
+    section.style.style = "margin-left: 270px;";
 }
 
 window.onload = afterLoad;
