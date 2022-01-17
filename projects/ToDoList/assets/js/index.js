c7864cc8bf4fa8195e16fa12b6949117b539f718
DainWs <jose.ant.du@gmail.com>
2022-01-17 09:40:24 +0100

    Project ToDoList

--- projects/ToDoList/assets/js/index.js
+++ projects/ToDoList/assets/js/index.js
@@ -7,8 +7,25 @@ const ToDoList = {
     },
     methods: {
         addTarea() {
-            this.tareas.push({ nombre: this.nuevaTarea, fecha: new Date() });
+            this.tareas.push({ nombre: this.nuevaTarea, fecha: new Date(), isCompleted: false });
             this.nuevaTarea = '';
+        },
+        formattedFecha(fecha) {
+            return `${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()} ${fecha.getDate()}/${fecha.getMonth()+1}/${fecha.getFullYear()}`;
+        },
+        turnCompleteState(tarea) {
+            tarea.isCompleted = !tarea.isCompleted;
+        },
+        getButtonText(tarea) {
+            return (tarea.isCompleted) ? 'Cancelar' : 'Completar';
+        },
+        isTaskCompletedText(tarea) {
+            return (tarea.isCompleted) ? 'Completo' : 'Incompleto' ;
+        },
+    },
+    computed: {
+        completedTask() {
+            return Array.from(this.tareas).filter((value) => value.isCompleted).length;
         }
     }
 }
